ee8591afb6155012cb57c2fe688fdae019d81e0f
thule <lunthu@gmail.com>
2019-05-29 10:44:23 +0700

    add componentDidMount for dogs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,10 @@ class App extends Component{
     photos: []
   }
 
+  componentDidMount(){
+    this.handleSubmitChange('dogs');
+  }
+
   handleSubmitChange = term => {
     axios
     .get(`https://api.unsplash.com/search/photos/?page=1&per_page=10&query=${term}&client_id=${KEY}`)
